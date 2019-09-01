680c94cc978af664677a9a30377c0110ccf35673
RihamAli <48959365+rihamAli-98@users.noreply.github.com>
2019-09-01 23:18:49 +0300

    Add files via upload

---
+++ First week.py
@@ -0,0 +1,47 @@
+#First Week
+
+#day-1-
+print("Hello World")
+if 5>4:
+    print("Five is greater than")
+
+#day-2-
+ #comment with one line
+"""
+Multiline
+"""
+
+#day-3-
+X,Y=5,10.5
+print(X)
+print(Y)
+print(X+Y)
+X="Riham"
+print("new value is: "+X)
+
+#day-4-
+A=9
+B=8.5
+C=10j
+D=4E2
+print(type(A))
+print(type(B))
+print(type(C))
+print(D)
+W=float(A)
+Z=int(B)
+print(type(W))
+print(type(Z))
+print(W)
+
+import random
+print(random.randrange(5,20))
+
+#day-5-
+x="apple"
+y="orange"
+z="limon"
+basket=x+y+z
+print(basket)
+
+
