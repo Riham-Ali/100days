9375f8b92156abba3966a40739adef3dd56ca3a8
RihamAli <48959365+rihamAli-98@users.noreply.github.com>
2019-09-06 02:33:13 +0300

    Add files via upload

---
+++ Challenge_Week.py
@@ -0,0 +1,15 @@
+# day-18-,-19-
+menu = ["coffe","cupcake","panCake"]
+x = menu.count("coffe")
+print(x)
+menu.append("Donut")
+menu.reverse()
+print(menu)
+menu.pop(2)
+print(menu)
+
+#************
+
+tuple=("swift","python","java")
+if "python" in tuple:
+    print("python in this Tuple")
